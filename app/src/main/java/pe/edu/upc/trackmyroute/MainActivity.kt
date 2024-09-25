7d32e78c8eff6127806d5e343d9bde54755befe0
Jorge <u201921734@upc.edu.pe>
2024-09-25 17:06:20 -0500

    chore: added views to main activity

--- app/src/main/java/pe/edu/upc/trackmyroute/MainActivity.kt
+++ app/src/main/java/pe/edu/upc/trackmyroute/MainActivity.kt
@@ -11,37 +11,43 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.room.Room
+import pe.edu.upc.trackmyroute.common.Constants
+import pe.edu.upc.trackmyroute.core.AppDatabase
+import pe.edu.upc.trackmyroute.data.remote.RouteService
+import pe.edu.upc.trackmyroute.data.repository.RouteRepository
+import pe.edu.upc.trackmyroute.presentation.tracking.RouteScreen
+import pe.edu.upc.trackmyroute.presentation.tracking.RouteViewModel
 import pe.edu.upc.trackmyroute.ui.theme.TrackMyRouteMobileAppTheme
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val service = Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(RouteService::class.java)
+
+        val dao = Room
+            .databaseBuilder(applicationContext, AppDatabase::class.java, "jokes-db")
+            .build()
+            .getRouteDao()
+
+        val repository = RouteRepository(service, dao)
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TrackMyRouteMobileAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    val routeViewModel = RouteViewModel(repository)
+                    RouteScreen(routeViewModel)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TrackMyRouteMobileAppTheme {
-        Greeting("Android")
-    }
-}
--- app/src/main/java/pe/edu/upc/trackmyroute/presentation/tracking/RouteScreen.kt
+++ app/src/main/java/pe/edu/upc/trackmyroute/presentation/tracking/RouteScreen.kt
@@ -1,12 +1,23 @@
 package pe.edu.upc.trackmyroute.presentation.tracking
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun RouteScreen(viewModel: RouteViewModel){
@@ -15,7 +26,11 @@ fun RouteScreen(viewModel: RouteViewModel){
             modifier = Modifier.fillMaxWidth().padding(paddingValues),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            OutlinedButton(onClick = {
 
+            }) {
+                Text("Ver Rutas")
+            }
         }
     }
 }
