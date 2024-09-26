921113e48d711622f3ec385de36e70c20237fba3
f4v10 <102195498+f4v10@users.noreply.github.com>
2024-09-26 00:49:07 -0500

    feat: added notifications screen on main activity.

--- app/src/main/java/pe/edu/upc/trackmyroute/MainActivity.kt
+++ app/src/main/java/pe/edu/upc/trackmyroute/MainActivity.kt
@@ -5,43 +5,50 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.room.Room
+import pe.edu.upc.trackmyroute.common.Constants
+import pe.edu.upc.trackmyroute.core.AppDatabase
+import pe.edu.upc.trackmyroute.data.remote.NotificationService
+import pe.edu.upc.trackmyroute.data.repository.NotificationRepository
+import pe.edu.upc.trackmyroute.presentation.NotificationListScreen
+import pe.edu.upc.trackmyroute.presentation.NotificationListViewModel
 import pe.edu.upc.trackmyroute.ui.theme.TrackMyRouteMobileAppTheme
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val service = Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(NotificationService::class.java)
+
+        val dao = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java,
+            "db-notifications"
+        ).build().getNotificationDao()
+
+        val repository = NotificationRepository(service, dao)
+
         enableEdgeToEdge()
         setContent {
             TrackMyRouteMobileAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val notificationViewModel = NotificationListViewModel(repository)
+                    NotificationListScreen(notificationViewModel)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TrackMyRouteMobileAppTheme {
-        Greeting("Android")
     }
 }
