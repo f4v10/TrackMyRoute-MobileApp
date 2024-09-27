064770ea9d37afd77e3f2cf5d321cf8243b51d1d
Jorge <u201921734@upc.edu.pe>
2024-09-27 14:25:58 -0500

    fear(sign-up): updated style to SignUpScreen.kt

--- app/src/main/java/pe/edu/upc/trackmyroute/feature_login/presentation/sign_up/SignUpScreen.kt
+++ app/src/main/java/pe/edu/upc/trackmyroute/feature_login/presentation/sign_up/SignUpScreen.kt
@@ -4,47 +4,94 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.skydoves.landscapist.ImageOptions
+import com.skydoves.landscapist.glide.GlideImage
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SignUpScreen(viewModel: SignUpViewModel) {
     val username = viewModel.username.value
     val password = viewModel.password.value
-
     val state = viewModel.state.value
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
 
     Scaffold (
         topBar = {
-            TopAppBar(
-                title = { Text("Track My Route") },
-                colors = TopAppBarDefaults.topAppBarColors(
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary
-                )
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(
+                        "Track My Route",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Home,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                scrollBehavior = scrollBehavior,
             )
-        }
-    ){ paddingValues ->
+        },
+        ){ paddingValues ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues),
             verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Text(text = "Crear Cuenta", fontSize = 30.sp)
+            GlideImage(
+                modifier = Modifier.size(80.dp),
+                imageModel = { "https://i.imgur.com/89bhrOi.png" }, // loading a network image using an URL.
+                imageOptions = ImageOptions(
+                    contentScale = ContentScale.Crop,
+                    alignment = Alignment.Center
+                ),
+            )
+
+            Text(text = "Crear Cuenta", fontSize = 30.sp, fontWeight = FontWeight.Bold, fontFamily = FontFamily.SansSerif)
 
             OutlinedTextField(value = username,
                 label = { Text("Usuario") },
@@ -59,7 +106,7 @@ fun SignUpScreen(viewModel: SignUpViewModel) {
             OutlinedButton(onClick = {
                 viewModel.signUp()
             }) {
-                Text(text = "Sign up")
+                Text(text = "Regístrate")
             }
             if (state.isLoading) {
                 CircularProgressIndicator()
